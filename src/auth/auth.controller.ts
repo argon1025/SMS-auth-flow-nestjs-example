d08b5daa5b5aba006e1f5b92f6285db5883e1706
argon1025 <argon1025@gmail.com>
2022-04-23 00:34:53 +0900

    feat: 인증번호 검증 verificationSms 엔드포인트

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,42 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Res } from '@nestjs/common';
+import { Response } from 'express';
 
-import { SendSms } from 'src/auth/auth.decorator';
+import { SendSms, VerificationSms } from 'src/auth/auth.decorator';
 import { AuthService } from 'src/auth/auth.service';
+import { TokenService } from 'library/jwt/token.service';
 
 import { SendSmsBodyRequestDto } from 'src/auth/dto/send-sms.dto';
+import { VerificationSmsBodyRequestDto } from 'src/auth/dto/verification-sms.dto';
+import { CookieService } from 'library/cookie/cookie.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly tokenService: TokenService,
+    private readonly cookieService: CookieService,
+  ) {}
 
   @SendSms()
   async sendSms(@Body() { phone }: SendSmsBodyRequestDto) {
     this.authService.sendSms(phone);
+    return null;
+  }
+
+  @VerificationSms()
+  async verificationSms(
+    @Body() { phone, code }: VerificationSmsBodyRequestDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    await this.authService.verificationSms(phone, code);
+    console.log(this.cookieService.getAuthCookieOptions());
+    const authToken = await this.tokenService.generateAuthToken({ phone });
+    response.cookie(
+      'authToken',
+      authToken,
+      this.cookieService.getAuthCookieOptions(),
+    );
+
     return null;
   }
 }
