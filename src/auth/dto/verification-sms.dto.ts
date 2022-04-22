8d908b041a3a54f10f2b296efe1bb44ecd197e1b
argon1025 <argon1025@gmail.com>
2022-04-23 00:34:18 +0900

    feat: 인증번호 검증 Dto, Decorator

---
+++ src/auth/dto/verification-sms.dto.ts
@@ -0,0 +1,26 @@
+import { Customers } from '@prisma/client';
+import { Transform, Type } from 'class-transformer';
+import {
+  IsInt,
+  IsMobilePhone,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class VerificationSmsBodyRequestDto {
+  // NOTE: 반드시 국가 코드를 입력해야합니다
+  @IsMobilePhone('ko-KR', { strictMode: true })
+  @IsString()
+  @Transform(({ value }) => {
+    return String(value).replace(/ |-/g, '').replace('+82010', '+8210');
+  })
+  readonly phone: Customers['phone'];
+
+  @IsNumber()
+  @Type(() => Number)
+  @Max(9899999)
+  @Min(999999)
+  readonly code: number;
+}
