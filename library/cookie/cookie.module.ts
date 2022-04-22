39921602c77acfb05604bd9ee738b292a18b0df9
argon1025 <argon1025@gmail.com>
2022-04-23 00:11:04 +0900

    feat: CookieService. Module, 환경변수 구성

---
+++ library/cookie/cookie.module.ts
@@ -0,0 +1,5 @@
+import { Module } from '@nestjs/common';
+import { CookieService } from 'library/cookie/cookie.service';
+
+@Module({ providers: [CookieService], exports: [CookieService] })
+export class CookieModule {}
