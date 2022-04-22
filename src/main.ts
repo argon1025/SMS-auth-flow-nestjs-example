04ac23af63c11f4aadb68db9794b87eae887d028
argon1025 <argon1025@gmail.com>
2022-04-22 21:17:16 +0900

    feat: Swagger OpanAPI 문서 빌더

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { swaggerDocumentBuilder } from 'library/swagger/swagger-document.bootstrap';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  swaggerDocumentBuilder(app);
   await app.listen(3000);
 }
 bootstrap();
