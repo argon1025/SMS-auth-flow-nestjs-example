0738a728df19de4d96e4a2db4c0a78cb072385ee
argon1025 <argon1025@gmail.com>
2022-04-23 00:11:50 +0900

    feat: cookieParser 미들웨어 등록

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+
 import { swaggerDocumentBuilder } from 'library/swagger/swagger-document.bootstrap';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   swaggerDocumentBuilder(app);
+
+  app.use(cookieParser());
+
   await app.listen(3000);
 }
 bootstrap();
