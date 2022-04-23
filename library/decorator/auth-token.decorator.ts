a6f9a50a145ed6dfbc1017b7675c69a8ee78c96b
argon1025 <argon1025@gmail.com>
2022-04-23 13:54:27 +0900

    feat: userAuthToken passport 인증 전략 추가

---
+++ library/decorator/auth-token.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { AuthTokenPayLoad } from 'library/jwt/type/auth-token-payload';
+
+export const JwtUserId = createParamDecorator(
+  (_: unknown, ctx: ExecutionContext): AuthTokenPayLoad => {
+    const request = ctx.switchToHttp().getRequest();
+    // TODO: 타입 단언 변경 필요
+    const tokenPayload = request.user as AuthTokenPayLoad;
+
+    return { phone: tokenPayload.phone };
+  },
+);
