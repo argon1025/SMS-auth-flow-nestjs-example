777322d998383cfc6aaa280c2be7e1437f8af8d2
argon1025 <argon1025@gmail.com>
2022-04-25 23:51:28 +0900

    feat: Costomer Module DI

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaModule } from 'library/prisma/prisma.module';
+import { CustomersRepository } from 'src/customers/customers.repository';
 import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
 
 @Module({
   imports: [PrismaModule],
-  providers: [CustomersService],
+  providers: [CustomersService, CustomersRepository],
   controllers: [CustomersController],
 })
 export class CustomersModule {}
