fdd42845221812a8c016a75170b5781f7989e2ae
argon1025 <argon1025@gmail.com>
2022-04-25 23:49:40 +0900

    feat: 내정보 출력을 위한 Repository

--- src/customers/customers.repository.ts
+++ src/customers/customers.repository.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Customers } from '@prisma/client';
+
+import { PrismaService } from 'library/prisma/prisma.service';
 
 @Injectable()
-export class CustomersRepository {}
+export class CustomersRepository {
+  findFirstById({
+    prismaService,
+    id,
+  }: {
+    prismaService: PrismaService;
+    id: Customers['id'];
+  }) {
+    return prismaService.customers.findUnique({ where: { id } });
+  }
+}
