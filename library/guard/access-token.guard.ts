c975fcbeed6b54346004d68dde28ad97ac04a873
argon1025 <argon1025@gmail.com>
2022-04-25 16:24:40 +0900

    fix: 액세스 토큰 가드 잘못된 이름

--- library/guard/access-token.guard.ts
+++ library/guard/access-token.guard.ts
@@ -7,11 +7,11 @@ import {
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-class JwtRefreshToken extends AuthGuard('access-token') {
+class JwtAccessToken extends AuthGuard('access-token') {
   // NOTE: 에러를 핸들링 한다, 유저가 존재하지 않을 경우 null 데이터를 그대로 리턴한다
   handleRequest<TUser>(err: Error, user: TUser) {
     if (err) throw new ForbiddenException();
     return user;
   }
 }
-export const JwtRefreshTokenGuard = () => UseGuards(JwtRefreshToken);
+export const JwtAccessTokenGuard = () => UseGuards(JwtAccessToken);
