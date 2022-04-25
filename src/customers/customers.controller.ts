95230b6b9c03cf60f1fcf091433a9f0542ac7b06
argon1025 <argon1025@gmail.com>
2022-04-25 23:51:18 +0900

    feat: 내정보 엔드포인트

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UnauthorizedException } from '@nestjs/common';
+import { JwtAccessTokenData } from 'library/decorator/access-token.decorator';
+import { AccessTokenPayLoad } from 'library/jwt/type/access-token-payload';
+import { GetMe } from 'src/customers/customers.decorator';
+import { CustomersService } from 'src/customers/customers.service';
+import { GetMeResponseDto } from 'src/customers/dto/get-me.dto';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+  constructor(private readonly customerService: CustomersService) {}
+
+  @GetMe()
+  async getMe(@JwtAccessTokenData() { id }: AccessTokenPayLoad) {
+    if (!id) throw new UnauthorizedException();
+    const { name, nickname, email, phone, joinedAt } =
+      await this.customerService.getCustomerInfo(id);
+    return new GetMeResponseDto({ name, nickname, email, phone, joinedAt });
+  }
+}
