2bf46ff2740df5a8cdc9498105e67e6ab0664189
argon1025 <argon1025@gmail.com>
2022-04-26 19:52:06 +0900

    refactor: 사용하지않는 가져오기 제거

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -19,7 +19,6 @@ import {
   AlreadyJoinedError,
   MismatchVerificationError,
   MismatchVerificationPasswordError,
-  NeedCreateFirstError,
   NeedJoinError,
   NotFoundCustomerError,
   NotFoundVerificationError,
