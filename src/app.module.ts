356aefb6e66ea48841f39975f6a10de15082de41
argon1025 <argon1025@gmail.com>
2022-04-26 19:39:32 +0900

    refactor: provider 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import {
   BadRequestException,
   ClassSerializerInterceptor,
   Module,
+  Provider,
   ValidationPipe,
 } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
@@ -16,6 +17,36 @@ import { CustomersModule } from './customers/customers.module';
 /* eslint-disable import/order */
 import { ExceptionMessageInterface } from 'library/exception/type/custom-exception-message.type';
 
+const classSerializerInterceptorProvider: Provider = {
+  provide: APP_INTERCEPTOR,
+  useClass: ClassSerializerInterceptor,
+};
+
+const validationPipeProvider: Provider = {
+  provide: APP_PIPE,
+  useValue: new ValidationPipe({
+    // NOTE: JSON 페이로드를 DTO 프로퍼티에 지정된 타입으로 변환합니다
+    transform: true,
+    // NOTE: validation 데코레이터가 없는 모든 프로퍼티에 제거한다
+    whitelist: true,
+    // NOTE: 알수없는 프로퍼티가 유효성 검사를 통과하는것을 막습니다
+    forbidUnknownValues: true,
+    exceptionFactory: (errors: ValidationError[]) => {
+      if (!errors[0]?.constraints) return new BadRequestException();
+      const firstKey = Object.keys(errors[0].constraints);
+      const errorMessage: ExceptionMessageInterface = {
+        [CountryCode.EN]: errors[0].constraints[`${firstKey}`],
+      };
+      return new BadRequestException(errorMessage);
+    },
+  }),
+};
+
+const allExceptionsFilterProvider: Provider = {
+  provide: APP_FILTER,
+  useClass: AllExceptionsFilter,
+};
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -26,27 +57,9 @@ import { ExceptionMessageInterface } from 'library/exception/type/custom-excepti
     CustomersModule,
   ],
   providers: [
-    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
-    {
-      provide: APP_PIPE,
-      useValue: new ValidationPipe({
-        // NOTE: JSON 페이로드를 DTO 프로퍼티에 지정된 타입으로 변환합니다
-        transform: true,
-        // NOTE: validation 데코레이터가 없는 모든 프로퍼티에 제거한다
-        whitelist: true,
-        // NOTE: 알수없는 프로퍼티가 유효성 검사를 통과하는것을 막습니다
-        forbidUnknownValues: true,
-        exceptionFactory: (errors: ValidationError[]) => {
-          if (!errors[0]?.constraints) return new BadRequestException();
-          const firstKey = Object.keys(errors[0].constraints);
-          const errorMessage: ExceptionMessageInterface = {
-            [CountryCode.EN]: errors[0].constraints[`${firstKey}`],
-          };
-          return new BadRequestException(errorMessage);
-        },
-      }),
-    },
-    { provide: APP_FILTER, useClass: AllExceptionsFilter },
+    classSerializerInterceptorProvider,
+    validationPipeProvider,
+    allExceptionsFilterProvider,
   ],
 })
 export class AppModule {}
