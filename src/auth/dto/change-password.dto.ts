2426443b78dabee5aa7b1c6ad9850a21c0e2203c
argon1025 <argon1025@gmail.com>
2022-04-26 19:27:54 +0900

    feat: Validation, 유효한 입력인지 확인

--- src/auth/dto/change-password.dto.ts
+++ src/auth/dto/change-password.dto.ts
@@ -3,6 +3,6 @@ import { IsString, Length } from 'class-validator';
 
 export class ChangePasswordBodyRequestDto {
   @IsString()
-  @Length(1, 30)
+  @Length(7, 30)
   readonly password: Customers['password'];
 }
